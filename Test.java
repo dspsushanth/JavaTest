28811e210255f5e8a6940eb112d3e93fa1896555
dspsushanth <sai61095@gmail.com>
2017-08-27 13:47:10 +0530

    Initial Commit

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+	System.out.println ("Test!");
+    }
+    // Working kadha
+    // Project create chesinapudu default package lo pettav
+    // Ila create cheyali ok?
+    // ok ante ippud okasare cheystha chudu ila cheyoakch ane
+    //ok cheyyi choosta and location marchu project create chesinapudu epudanna
+    // Edaina drive lo peetko
+    // malla cheyyi
+    
+    
+    
+}
